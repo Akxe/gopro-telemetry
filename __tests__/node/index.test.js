bc07bfd26fd056dc749400c7025b2aa1568f1701
Adam Eisenreich <akxe@seznam.cz>
2023-06-12 14:44:33 +0200

    Fracture tests to make them more clear

--- __tests__/node/index.test.js
+++ __tests__/node/index.test.js
@@ -223,9 +223,24 @@ describe('Testing reusing parsed data', () => {
     result.push(await goproTelemetry({ parsedData, timing }));
   });
 
-  test(`Reused parsed data should output the same as binary data`, () => {
-    expect(JSON.stringify(result[0])).toBe(JSON.stringify(result[1]));
-  });
+  const keys = [
+    'ACCL', 'GYRO', 'SHUT',
+    'WBAL', 'WRGB', 'ISOE',
+    'YAVG', 'UNIF', 'SCEN',
+    'HUES', 'GPS5', 'GPS9',
+    'CORI', 'IORI', 'GRAV',
+    'WNDM', 'MWET', 'AALP',
+    'MSKP', 'LSKP',
+  ];
+  for (const key of keys) {
+    test(`Reused parsed data should output the same as binary data for "${key}"`, () => {
+      //console.log(result[0][1].streams[key]].samples[0]);
+
+      //console.log(key, result[0][1].streams[key].samples.slice(0, 3));
+      expect(result[0][1].streams[key].samples.slice(0, 3)).toEqual(result[1][1].streams[key].samples.slice(0, 3));
+      expect(false).toBe(true);
+    });
+  }
 });
 
 describe('Testing using new GPS9 stream', () => {
